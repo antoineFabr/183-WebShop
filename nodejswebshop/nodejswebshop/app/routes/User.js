8507063f4033f263a23e78fab38d589f5c68afd2
antoineFabr <163823977+antoineFabr@users.noreply.github.com>
2025-01-13 15:44:16 +0100

    ajout de la partie frontend

--- nodejswebshop/nodejswebshop/app/routes/User.js
+++ nodejswebshop/nodejswebshop/app/routes/User.js
@@ -4,3 +4,6 @@ const router = express.Router();
 const controller = require("../controllers/UserController");
 router.get('/', controller.get) 
 module.exports = router; 
+document.getElementById("bouton").onclick = function() {
+    document.getElementById("bouton").innerHTML = "click"
+};
