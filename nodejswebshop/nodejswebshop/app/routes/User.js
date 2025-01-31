2aac9c3c48e5452e96a7e18a96db282b10518a55
antoineFabr <163823977+antoineFabr@users.noreply.github.com>
2025-01-31 12:15:59 +0100

    lien de la db avec le code + hash du pw

--- nodejswebshop/nodejswebshop/app/routes/User.js
+++ nodejswebshop/nodejswebshop/app/routes/User.js
@@ -1,14 +1,47 @@
 const express = require("express");
-const mysql = require("mysql2");
+const crypto = require("crypto");
 const router = express.Router();
 const controller = require("../controllers/UserController");
 //router.get("/", controller.get);
 module.exports = router;
+
+var mysql = require("mysql2");
+var connection = mysql.createConnection({
+  host: "db_container",
+  user: "root",
+  password: "root",
+  database: "db_login",
+  port: 3306,
+});
+
 router.get("/login", (req, res) => {
   res.render("login", { name: "Antoine" });
 });
 
-router.post("/menu", (req, res) => {
+router.get("/register", (req, res) => {
+  res.render("register", { name: "nameless" });
+});
+router.get("/menu", (req, res) => {
   console.log(req.body);
   res.render("menu", { name: req.body.mail });
+});
+
+router.post("/login", (req, res) => {
+  connection.connect();
+  const user = req.body.mail.toString();
+  const query = `SELECT `;
+});
+router.post("/register", (req, res) => {
+  connection.connect();
+  const salt = crypto.randomBytes(8).toString("hex");
+  console.log(salt);
+  const cryptedPw = crypto.hash("sha256", req.body.pw + salt);
+
+  const query = `INSERT INTO T_user (addresseMail, motDePasse, sel) VALUES (?, ?, ?);`;
+  const values = [req.body.mail, cryptedPw, salt];
+
+  connection.query(query, values);
+  res.redirect("http://localhost:8080/user/menu");
+
+  connection.end();
 });
--- nodejswebshop/nodejswebshop/script.sql
+++ nodejswebshop/nodejswebshop/script.sql
@@ -1,9 +1,11 @@
 CREATE DATABASE db_login;
 /*ensuite executer ce script*/
+use db_login;
 CREATE TABLE T_user(
    userId INT AUTO_INCREMENT,
    addresseMail VARCHAR(50),
    motDePasse VARCHAR(50),
-   PRIMARY KEY(userId)
+   PRIMARY KEY(userId),
+   sel VARCHAR(50)
 );
 
